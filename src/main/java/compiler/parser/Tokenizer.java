61e3cef0528cefd1b4a542b345311d266c7b91bc
Dhyey <dhyeyshah4@gmail.com>
2020-08-27 12:27:38 +0530

    lexer:scanNum issue resolved

--- src/main/java/compiler/parser/Tokenizer.java
+++ src/main/java/compiler/parser/Tokenizer.java
@@ -81,7 +81,7 @@ public class Tokenizer {
 		while(true) {
 			reader.scanChar();
 			switch(reader.ch) {
-			case '1': case '2': case '3': case '4':
+			case '0': case '1': case '2': case '3': case '4':
             case '5': case '6': case '7': case '8': case '9':
             	reader.putChar(reader.ch);
             	continue;
