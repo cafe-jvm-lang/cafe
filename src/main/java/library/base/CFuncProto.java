2eafd47cfa1104d5cba02d437aabc7d0bd356ee7
Dhyey <dhyeyshah4@gmail.com>
2021-02-21 15:39:21 +0530

    bind method added

--- src/main/java/library/base/CFuncProto.java
+++ src/main/java/library/base/CFuncProto.java
@@ -29,5 +29,44 @@
 
 package library.base;
 
+import library.DFunc;
+import library.DObject;
+import runtime.DObjectCreator;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+
 public class CFuncProto {
+    public static DFunc bind(DObject func, DObject thisP, Object... args) {
+        MethodHandle h1 = ((DFunc) func).handle();
+        MethodHandle h2 = null;
+        boolean isVarity = ((DFunc) func).isVarity();
+
+        if (thisP == null && args.length > 0)
+            // this is required as arguments array is not spread when thisP is null.
+            // args = { args[] }
+            args = (Object[]) args[0];
+
+        if (thisP != null) {
+            h2 = h1.bindTo(thisP);
+        }
+
+        if (args.length > 0) {
+            int pos = 1;
+            MethodHandle mh = h1;
+            if (thisP != null) {
+                pos = 0;
+                mh = h2;
+            }
+            h2 = MethodHandles.insertArguments(mh, pos, args);
+        }
+
+        if (h2 != null) {
+            if (isVarity)
+                h2 = h2.asVarargsCollector(Object[].class);
+            return DObjectCreator.createFunc(h2);
+        }
+
+        return (DFunc) func;
+    }
 }
