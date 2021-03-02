69190f2b40c4f28b5ada121c0f020edf4e93e3a0
Dhyey Shah <dhyeyshah4@gmail.com>
2021-03-02 10:36:42 +0530

    Update innoinstall.sh

--- gradle/innosetup/innoinstall.sh
+++ gradle/innosetup/innoinstall.sh
@@ -5,7 +5,7 @@ rm -rf /tmp/inno
 mkdir /tmp/inno
 cd /tmp/inno
 
-wget -O is.exe https://jrsoftware.org/download.php/is.exe
+wget -O is.exe http://files.jrsoftware.org/is/5/isetup-5.5.5.exe
 innoextract is.exe
 mkdir -p ~/".wine/drive_c/inno"
 cp -R app/* ~/".wine/drive_c/inno"
